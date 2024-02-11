0a0726fac444388f37ca04a35c377380253116b7
Obi Pascal <pascalobi83@gmail.com>
2024-02-11 14:17:29 +0100

    updated

--- app/Models/Misc/Reviews.php
+++ app/Models/Misc/Reviews.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Misc;
 
+use App\Models\Account\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "account_id", "account_id");
+    }
 }
